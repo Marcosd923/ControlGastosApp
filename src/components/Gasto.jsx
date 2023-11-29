9f285e4392af660f9380d8f8e2e7570933eeaeb6
Marcos923 <marcos.daut@gmail.com>
2023-11-29 20:13:51 -0300

    creando el componente para crear un gasto

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,9 @@
+const Gasto = () => {
+  return (
+    <div>
+      <p>Desde gasto-jsx </p>
+    </div>
+  );
+};
+
+export default Gasto;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -67,12 +67,12 @@ const Modal = ({ setModal, animarModal, setAnimarModal, guardarGasto }) => {
             onChange={(e) => setCategoria(e.target.value)}
           >
             <option value="">--Seleccione--</option>
-            <option value="ahorro">--Ahorro--</option>
-            <option value="comida">--Comida--</option>
-            <option value="casa">--Gasa--</option>
-            <option value="gastos">--Gastos Varios--</option>
-            <option value="salud">--Salud--</option>
-            <option value="suscripciones">--Suscripciones--</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="gastos">Gastos Varios</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
           </select>
         </div>
         <input type="submit" value="Añadir Gasto" />
