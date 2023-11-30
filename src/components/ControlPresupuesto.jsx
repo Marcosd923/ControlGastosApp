70718aef328edf57c7c9e2481ad1525a24958b1e
Marcos923 <marcos.daut@gmail.com>
2023-11-30 18:45:14 -0300

    calculando el presupuesto gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,18 @@
-const ControlPresupuesto = ({ presupuesto }) => {
+import { useState, useEffect } from "react";
+
+const ControlPresupuesto = ({ presupuesto, gastos }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0
+    );
+
+    setGastado(totalGastado);
+  }, [gastos]);
+
   const formatearCantidad = (cantidad) => {
     return cantidad.toLocaleString("en-US", {
       style: "currency",
@@ -17,10 +31,10 @@ const ControlPresupuesto = ({ presupuesto }) => {
           <span>Presupuesto :</span> {formatearCantidad(presupuesto)}
         </p>
         <p>
-          <span>Disponible :</span> {formatearCantidad(0)}
+          <span>Disponible :</span> {formatearCantidad(disponible)}
         </p>
         <p>
-          <span>Gastado:</span> {formatearCantidad(0)}
+          <span>Gastado:</span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
